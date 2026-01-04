5d3ef5a1854627b7f8df2fc4cf125b3e3da769c5
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2026-01-04 06:10:54 +0530

    fix: printing generated onion url in logs during startup

--- start-tor.sh
+++ start-tor.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# Print Tor Onion URL to the console in purple color
+# The value is stored in the file /var/lib/tor/hidden_service/hostname
+echo -e "\033[35mTor Onion URL: \033[0m$(cat /var/lib/tor/hidden_service/hostname)"
+
+# Start tor
 tor -f /etc/tor/torrc &
